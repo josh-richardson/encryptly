774425d59be73a3ec15ebd92d782692eb3edc6d6
Joshua Richardson <joshua@richardson.tech>
2017-03-03 16:19:55 +0000

    Fixes template and staticfiles

--- encryptly/encryptly/settings.py
+++ encryptly/encryptly/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'encryptly.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
--- encryptly/encryptly_backend/views.py
+++ encryptly/encryptly_backend/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "encryptly_backend/base.html", {})
